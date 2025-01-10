85414973ac8c6fe33dd123613819ca3c50b28464
MOVE2U <doubleflesh@naver.com>
2025-01-10 21:37:54 +0900

    스폰데이터 입력

--- Assets/Undead Survivor/Codes/Spawner.cs
+++ Assets/Undead Survivor/Codes/Spawner.cs
@@ -2,20 +2,25 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+    // Transform 컴포넌트를 배열에 담는데
     public Transform[] spawnPoint;
+    public SpawnData[] spawnData;
 
+    int level;
     float timer;
-
+    
     void Awake()
     {
+        // 현재 오브젝트와 그 자식들의 Transform 컴포넌트를 가져온다.
         spawnPoint= GetComponentsInChildren<Transform>();
     }
     // Update is called once per frame
     void Update()
     {
         timer += Time.deltaTime;
+        level = Mathf.FloorToInt(GameManager.instance.gameTime / 10f);
 
-        if(timer > 0.2f)
+        if (timer > 0.2f)
         {
             timer = 0;
             Spawn();
@@ -23,9 +28,19 @@ public class Spawner : MonoBehaviour
 
         void Spawn()
         {
-            GameObject enemy = GameManager.instance.pool.Get(Random.Range(0, 2));
+            GameObject enemy = GameManager.instance.pool.Get(level);
+            // GetComponentsInChildren은 현재 오브젝트도 가져오기 때문에 0은 현재 오브젝트가 된다. 자식만 가져오기 위해 1부터 시작한것
             enemy.transform.position = spawnPoint[Random.Range(1, spawnPoint.Length)].position;
         }
 
     }
+}
+
+[System.Serializable]
+public class SpawnData
+{
+    public int spriteType;
+    public float spawnTime;
+    public int health;
+    public float speed;
 }
