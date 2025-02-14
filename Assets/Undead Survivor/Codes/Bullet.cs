87af423c14a2ad7d77616b77dbbb151cab724851
MOVE2U <doubleflesh@naver.com>
2025-02-14 19:14:02 +0900

    원거리무기

--- Assets/Undead Survivor/Codes/Bullet.cs
+++ Assets/Undead Survivor/Codes/Bullet.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
@@ -5,9 +6,34 @@ public class Bullet : MonoBehaviour
     public float damage;
     public int per;
 
-    public void Init(float damage, int per)
+    Rigidbody rigid;
+
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    public void Init(float damage, int per, Vector3 dir)
     {
         this.damage = damage;
         this.per = per;
+
+        if(per > -1)
+        {
+            rigid.linearVelocity = dir;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!collision.CompareTag("Enemy") || per == -1)
+            return;
+
+        per--;
+        // 최초 per == -1인건 return을 했지만, 코드는 순차적으로 실행되기 때문에 per --를 한 이후에 다시 확인한 if문에서는 per == -1이 있을 수 있다.
+        if (per==-1)
+        {
+            gameObject.SetActive(false);
+        }
     }
 }
--- Assets/Undead Survivor/Codes/Weapon.cs
+++ Assets/Undead Survivor/Codes/Weapon.cs
@@ -8,6 +8,14 @@ public class Weapon : MonoBehaviour
     public int count;
     public float speed;
 
+    float timer;
+    Player player;
+
+    private void Awake()
+    {
+        player = GetComponentInParent<Player>();
+    }
+
     private void Start()
     {
         Init();
@@ -22,6 +30,13 @@ public class Weapon : MonoBehaviour
                 transform.Rotate(Vector3.back * speed * Time.deltaTime);
                 break;
             default:
+                timer += Time.deltaTime;
+
+                if(timer > speed)
+                {
+                    timer = 0;
+                    Fire();
+                }
                 break;
         }
         if(Input.GetKeyDown(KeyCode.Space))
@@ -32,12 +47,15 @@ public class Weapon : MonoBehaviour
 
     public void LevelUp(float damage, int count)
     {
-        this.damage += damage;
-        this.count += count;
-
-        if (id == 0)
+        switch(id)
         {
-            Batch();
+            case 0:
+                this.damage += damage;
+                this.count += count;
+                Batch();
+                break;
+            default:
+                break;
         }
     }
 
@@ -50,37 +68,53 @@ public class Weapon : MonoBehaviour
                 Batch();
                 break;
             default:
+                speed = 0.3f;
                 break;
         }
     }
 
     void Batch()
     {
-        for (int index = 0; index < count; index++)
+        switch (id)
         {
-            Transform bullet;
+            case 0:
+                for (int index = 0; index < count; index++)
+                {
+                    Transform bullet;
 
-            if (index < transform.childCount)
-            {
-                bullet = transform.GetChild(index);
-            }
-            else
-            {
-                bullet = GameManager.instance.pool.Get(prefabId).transform;
-                bullet.parent = transform;
-            }
-            // 초기화
-            bullet.localPosition = Vector3.zero;
-            bullet.localRotation = Quaternion.identity;
+                    if (index < transform.childCount)
+                    {
+                        bullet = transform.GetChild(index);
+                    }
+                    else
+                    {
+                        bullet = GameManager.instance.pool.Get(prefabId).transform;
+                        bullet.parent = transform;
+                    }
+                    // 초기화
+                    bullet.localPosition = Vector3.zero;
+                    bullet.localRotation = Quaternion.identity;
 
-            bullet.Rotate(Vector3.forward * index * 360 / count);
-            // Vector3.up은 월드 좌표계 기준 위쪽. Translate.up(여기선 bullet.up을 사용)은 로컬 좌표계 기준.
-            // Translate의 두번째 인자는 월드 좌표계, 로컬 좌표계를 결정. Space.World 또는 Space.Self.
-            // 여기선 이미 bullet.up에서 로컬 좌표계를 사용했기 때문에, Space.World를 사용해도, Space.Self를 사용해도 같은 결과가 나온다.
-            bullet.Translate(bullet.up * 1.5f, Space.World);
-            // bullet은 GameObject형 변수가 아니라 컴포넌트인 Transform임. 그럼에도 불구하고 다른 컴포넌트에 아래처럼 접근할 수 있음.
-            // 즉, GetComponent<t>()는 다른 컴포넌트에서도 접근할 수 있음.
-            bullet.GetComponent<Bullet>().Init(damage, -1); // -1 is Infinity Per.
+                    bullet.Rotate(Vector3.forward * index * 360 / count);
+                    // Vector3.up은 월드 좌표계 기준 위쪽. Translate.up(여기선 bullet.up을 사용)은 로컬 좌표계 기준.
+                    // Translate의 두번째 인자는 월드 좌표계, 로컬 좌표계를 결정. Space.World 또는 Space.Self.
+                    // 여기선 이미 bullet.up에서 로컬 좌표계를 사용했기 때문에, Space.World를 사용해도, Space.Self를 사용해도 같은 결과가 나온다.
+                    bullet.Translate(bullet.up * 1.5f, Space.World);
+                    // bullet은 GameObject형 변수가 아니라 컴포넌트인 Transform임. 그럼에도 불구하고 다른 컴포넌트에 아래처럼 접근할 수 있음.
+                    // 즉, GetComponent<t>()는 다른 컴포넌트에서도 접근할 수 있음.
+                    bullet.GetComponent<Bullet>().Init(damage, -1); // -1 is Infinity Per.
+                }
+                break;
+            default:
+                break;
         }
+    }
+
+    void Fire()
+    {
+        if (!player.scanner.nearestTarget)
+            return;
+        Transform bullet = GameManager.instance.pool.Get(prefabId).transform;
+        bullet.position = transform.position;
     }
 }
